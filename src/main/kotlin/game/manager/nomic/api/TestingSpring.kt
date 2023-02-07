30839be33e2b5460415a99dde7c24fdb1cc49322
Tyler Marefke <tyler.marefke@gmail.com>
2023-02-07 09:11:45 -0600

    Linting

--- src/main/kotlin/game/manager/nomic/api/TestingSpring.kt
+++ src/main/kotlin/game/manager/nomic/api/TestingSpring.kt
@@ -1,6 +1,5 @@
 package game.manager.nomic.api
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
