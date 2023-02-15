3bd8b007519bad7871248b0c5dda903b2a322cfd
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-15 15:50:09 -0600

    Alters PasswordHash underlying type to direct string and adds clarifying comment for the regex

--- src/main/kotlin/nomic/domain/entities/Credential.kt
+++ src/main/kotlin/nomic/domain/entities/Credential.kt
@@ -14,8 +14,9 @@ value class LoginName(@JsonValue val rawName: String) {
 }
 
 @JvmInline
-value class PasswordHash(val rawHash: CharSequence) {
+value class PasswordHash(val rawHash: String) {
     init {
+        // This regex validates that the string is an Argon2 hash encoded with the modular PHC format.
         val regex = Regex("^\\\$argon2(i?d?)\\\$v=(\\d+)\\\$m=(\\d+)\\\$t=(\\d+)\\\$p=(\\d+)\\\$([a-zA-Z\\-=\\/\\\\+]+)\\\$([a-zA-Z\\-=\\/\\\\+]+)\$")
         if (!regex.matches(rawHash)) {
             TODO("Add proper exception")
