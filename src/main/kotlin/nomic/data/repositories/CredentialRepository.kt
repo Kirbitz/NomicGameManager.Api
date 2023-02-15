976ced9760cded5828979e4e0140cd97ca9a6d28
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-15 15:01:20 -0600

    Adds skeleton implementations of the user and credential repositories

--- src/main/kotlin/nomic/data/repositories/CredentialRepository.kt
+++ src/main/kotlin/nomic/data/repositories/CredentialRepository.kt
@@ -4,6 +4,7 @@ import nomic.domain.entities.Credential
 import nomic.domain.entities.PasswordHash
 import nomic.domain.entities.User
 import nomic.domain.entities.Username
+import org.springframework.stereotype.Component
 
 
 interface CredentialRepository {
@@ -15,3 +16,22 @@ interface CredentialRepository {
 
 
 // TODO: Implement Credentials Repository once the database is setup
+@Component
+class CredentialRepositoryImpl : CredentialRepository{
+    override fun create(user: User, passwordHash: PasswordHash): Credential {
+        TODO("Not yet implemented")
+    }
+
+    override fun changePassword(credential: Credential, newHashedPassword: PasswordHash) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getByUser(user: User): Credential {
+        TODO("Not yet implemented")
+    }
+
+    override fun getByName(username: Username): Credential {
+        TODO("Not yet implemented")
+    }
+
+}
--- src/main/kotlin/nomic/data/repositories/UserRepository.kt
+++ src/main/kotlin/nomic/data/repositories/UserRepository.kt
@@ -2,6 +2,7 @@ package nomic.data.repositories
 
 import nomic.domain.entities.User
 import nomic.domain.entities.Username
+import org.springframework.stereotype.Component
 
 interface UserRepository {
     fun create(name: Username) : User
@@ -13,4 +14,24 @@ interface UserRepository {
 
     // get games that user is hosting?
     // get games that user is part of?
+}
+
+@Component
+class UserRepositoryImpl : UserRepository {
+    override fun create(name: Username): User {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserById(id: Int): User {
+        TODO("Not yet implemented")
+    }
+
+    override fun findUserByName(name: Username): User {
+        TODO("Not yet implemented")
+    }
+
+    override fun changeUsername(newName: Username) {
+        TODO("Not yet implemented")
+    }
+
 }
