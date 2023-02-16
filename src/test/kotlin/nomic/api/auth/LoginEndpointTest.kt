9f970bdfbbb82d5abddd9f5c3f1981b5b9832d4d
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-16 16:10:24 -0600

    Adds initial tests for login endpoint

---
+++ src/test/kotlin/nomic/api/auth/LoginEndpointTest.kt
@@ -0,0 +1,57 @@
+package nomic.api.auth
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Condition
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import java.util.*
+import java.util.function.Predicate
+
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+class LoginEndpointTest(@Autowired val client: TestRestTemplate) {
+
+    private val hasToken = Condition<LoginResponseModel?>(Predicate { it.token != null }, "Login Response has Token")
+
+    @Test
+    fun test_loginFails_noCredentials() {
+        val entity = client.getForEntity<LoginResponseModel>("/api/auth/login")
+        Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.FORBIDDEN)
+        Assertions.assertThat(entity.body).isNull()
+    }
+
+    @Test
+    fun test_loginFails_badCredentials() {
+        val headers = HttpHeaders()
+        val creds = Base64.getEncoder().encodeToString("baduser:badpassword".toByteArray())
+        headers.set("Authorization", "Basic $creds")
+
+        val request = HttpEntity<Any>(null, headers)
+        val entity = client.postForEntity("/api/auth/login", request, LoginResponseModel::class.java)
+        Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(entity.body).isNotNull().isNot(hasToken)
+    }
+
+    @Test
+    fun test_loginSucceeds() {
+        val headers = HttpHeaders()
+        // TODO Add credentials
+        val creds = Base64.getEncoder().encodeToString("".toByteArray())
+        headers.set("Authorization", "Basic $creds")
+
+        val request = HttpEntity<Any>(null, headers)
+        val entity = client.postForEntity("/api/auth/login", request, LoginResponseModel::class.java)
+        Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(entity.body).isNotNull().has(hasToken)
+    }
+}
