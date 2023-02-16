baeafb615008a0c8fb5c1e242e8d36a5dd20c5e3
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-16 13:56:48 -0600

    Refactors the login endpoint to input the credentials via basic authentication rather than the body

--- src/main/kotlin/nomic/api/auth/LoginEndpoint.kt
+++ src/main/kotlin/nomic/api/auth/LoginEndpoint.kt
@@ -2,27 +2,37 @@ package nomic.api.auth
 
 import nomic.domain.auth.UserAuthenticator
 import nomic.domain.entities.LoginName
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.Base64
 
 @RestController
 @RequestMapping("api/auth")
 class LoginEndpoint(val userAuthenticator: UserAuthenticator) {
 
     @GetMapping("login")
-    @ResponseBody
-    fun loginRequest(@RequestBody request: LoginRequestModel): LoginResponseModel {
+    fun loginRequest(@RequestHeader(HttpHeaders.AUTHORIZATION)
+                     authorization: String): ResponseEntity<LoginResponseModel> {
+        val credentialBytes = Base64.getDecoder().decode(authorization.substring("Basic ".length))
+        val credentials = String(credentialBytes).split(':')
+
         val userAuthentication = userAuthenticator.authenticateUserWithCredentials(
-            LoginName(request.name),
-            request.password,
+            LoginName(credentials[0]),
+            credentials[1],
         )
 
-        return LoginResponseModel(userAuthentication.isSuccess, userAuthentication.token)
+        val responseModel = LoginResponseModel(userAuthentication.isSuccess, userAuthentication.token)
+        return ResponseEntity(responseModel, HttpStatus.OK)
+
     }
 }
 
-data class LoginRequestModel(val name: String, val password: String)
 data class LoginResponseModel(val isSuccess: Boolean, val token: String? = null)
