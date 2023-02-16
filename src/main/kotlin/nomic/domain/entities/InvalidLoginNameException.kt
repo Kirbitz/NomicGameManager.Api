f9c335819d482327b8fbfa4b0f749e2b6040abc8
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-16 17:07:17 -0600

    Adds proper exception for bad username

--- src/main/kotlin/nomic/domain/entities/InvalidLoginNameException.kt
+++ src/main/kotlin/nomic/domain/entities/InvalidLoginNameException.kt
@@ -1,5 +1,5 @@
 package nomic.domain.entities
 
-class InvalidLoginNameException(name : String)
+class InvalidLoginNameException(name: String)
     : Exception("Invalid login name \'$name\': " +
         "Login names must contain only alphanumeric characters, dashes, or underscores.")
