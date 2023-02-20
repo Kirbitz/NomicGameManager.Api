a3bdac53658f91847082aa84fe69371d3335d9ce
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-20 11:35:12 -0600

    Refactors create user function to return a result containing a JWT Token as well

--- src/main/kotlin/nomic/domain/auth/UserAuthenticator.kt
+++ src/main/kotlin/nomic/domain/auth/UserAuthenticator.kt
@@ -4,7 +4,7 @@ import nomic.domain.entities.LoginName
 
 interface UserAuthenticator {
     fun authenticateUserWithCredentials(loginName: LoginName, password: String): AuthenticationResult
-    fun createUser(name: String, loginName: LoginName, password: String)
+    fun createUser(name: String, loginName: LoginName, password: String) : AuthenticationResult
 }
 
 data class AuthenticationResult(val isSuccess: Boolean, val token: String? = null)
