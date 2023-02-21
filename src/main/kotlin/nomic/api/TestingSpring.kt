4942cebcc6a15b1e427b5ef673cd0c6fda3a1743
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-21 16:01:11 -0600

    Cleans up temp testing endpoint

--- src/main/kotlin/nomic/api/TestingSpring.kt
+++ src/main/kotlin/nomic/api/TestingSpring.kt
@@ -1,14 +1,12 @@
 package nomic.api
 
-import nomic.data.DatabaseConfigProperties
-import org.ktorm.database.Database
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/hello")
-class TestingSpring(private val database: Database) {
+class TestingSpring {
     // Path to this is BASEURL/api/hello/springboot
 
     @GetMapping("springboot")
