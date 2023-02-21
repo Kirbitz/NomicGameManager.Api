e2f0578851fe7802b64e5a44fff0d23e68f79f8f
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-20 21:15:45 -0600

    Refactors the repository implementations into their own package

---
+++ src/main/kotlin/nomic/data/repositories/implementations/CredentialRepositoryImpl.kt
@@ -0,0 +1,72 @@
+package nomic.data.repositories.implementations
+
+import nomic.data.EntityNotFoundException
+import nomic.data.dtos.CredentialDTO
+import nomic.data.dtos.credentials
+import nomic.data.dtos.users
+import nomic.data.repositories.CredentialRepository
+import nomic.domain.entities.Credential
+import nomic.domain.entities.LoginName
+import nomic.domain.entities.PasswordHash
+import nomic.domain.entities.User
+import org.ktorm.database.Database
+import org.ktorm.dsl.eq
+import org.ktorm.entity.add
+import org.ktorm.entity.find
+import org.springframework.stereotype.Component
+import java.util.Optional
+
+@Component
+class CredentialRepositoryImpl(private val db: Database) : CredentialRepository {
+
+    override fun create(user: User, loginName: LoginName, passwordHash: PasswordHash): Credential {
+        var userDto = db.users.find { it.id eq user.id } ?: throw EntityNotFoundException(user.id)
+
+        val credential = CredentialDTO {
+            this.user = userDto
+            this.loginName = loginName.rawName
+            this.passwordHash = passwordHash.rawHash
+        }
+
+        db.credentials.add(credential)
+        return Credential(user, loginName, passwordHash)
+    }
+
+    override fun update(entity: Credential) {
+        val credDto = db.credentials.find { it.userId eq entity.user.id } ?: throw EntityNotFoundException(entity.id)
+
+        credDto.passwordHash = entity.passwordHash.rawHash
+        credDto.loginName = entity.loginName.rawName
+
+        credDto.flushChanges()
+    }
+
+    override fun getById(id: Int): Optional<Credential> {
+        val userDto = db.users.find { it.id eq id } ?: return Optional.empty()
+        val credDto = db.credentials.find { it.userId eq id } ?: return Optional.empty()
+
+        return Optional.of(
+            Credential(
+                User(id, userDto.name),
+                LoginName(credDto.loginName),
+                PasswordHash(credDto.passwordHash)
+            )
+        )
+    }
+
+    override fun delete(entity: Credential) {
+        db.credentials.find { it.userId eq entity.id }?.delete()
+    }
+
+    override fun getByName(loginName: LoginName): Optional<Credential> {
+        val credDto = db.credentials.find { it.loginName eq loginName.rawName } ?: return Optional.empty()
+        val user = User(credDto.user.id, credDto.user.name)
+        return Optional.of(
+            Credential(
+                user,
+                loginName,
+                PasswordHash(credDto.passwordHash)
+            )
+        )
+    }
+}
---
+++ src/main/kotlin/nomic/data/repositories/implementations/UserRepositoryImpl.kt
@@ -0,0 +1,45 @@
+package nomic.data.repositories.implementations
+
+import nomic.data.EntityNotFoundException
+import nomic.data.dtos.UserDTO
+import nomic.data.dtos.users
+import nomic.data.repositories.UserRepository
+import nomic.domain.entities.User
+import org.ktorm.database.Database
+import org.ktorm.dsl.eq
+import org.ktorm.entity.add
+import org.ktorm.entity.find
+import org.springframework.stereotype.Component
+import java.util.Optional
+
+@Component
+class UserRepositoryImpl(private val db: Database) : UserRepository {
+    override fun create(name: String): User {
+        val userDTO = UserDTO {
+            this.name = name
+        }
+
+        db.users.add(userDTO)
+        return User(userDTO.id, userDTO.name)
+    }
+
+    override fun findUserByName(name: String): Optional<User> {
+        val userDto = db.users.find { it.name eq name } ?: return Optional.empty()
+        return Optional.of(User(userDto.id, userDto.name))
+    }
+
+    override fun update(entity: User) {
+        val userDto = db.users.find { it.id eq entity.id } ?: throw EntityNotFoundException(entity.id)
+        userDto.name = entity.name
+        userDto.flushChanges()
+    }
+
+    override fun getById(id: Int): Optional<User> {
+        val userDto = db.users.find { it.id eq id } ?: return Optional.empty()
+        return Optional.of(User(userDto.id, userDto.name))
+    }
+
+    override fun delete(entity: User) {
+        db.users.find { it.id eq entity.id }?.delete()
+    }
+}
