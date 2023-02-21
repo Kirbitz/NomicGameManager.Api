406e6d53442ee1e909c11cc5587c8b6a0703d583
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-20 20:22:30 -0600

    Documents the KTorm entities for users and credentials

--- src/main/kotlin/nomic/data/dtos/CredentialDTO.kt
+++ src/main/kotlin/nomic/data/dtos/CredentialDTO.kt
@@ -7,6 +7,15 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
 
+/**
+ * This DTO represents a credential entry in the database and employs KTorm's framework.
+ *
+ * @see[org.ktorm.entity.Entity]
+ * @see[nomic.domain.entities.Credential]
+ * @property[user] Represents a foreign key connection to a user in the database
+ * @property[loginName] Represents a [nomic.domain.entities.LoginName][LoginName]
+ * @property[passwordHash] Represents a [nomic.domain.entities.PasswordHash][PasswordHash]
+ */
 interface CredentialDTO : Entity<CredentialDTO> {
     companion object : Entity.Factory<CredentialDTO>()
     var user: UserDTO
@@ -14,10 +23,21 @@ interface CredentialDTO : Entity<CredentialDTO> {
     var passwordHash: String
 }
 
+/**
+ * This object represents the credentials table in the database, employing KTorm's framework.
+ *
+ * @see[org.ktorm.schema.Table]
+ * @property[userId] The foreign key to the [Users] table and also the primary key for [CredentialDTO]
+ * @property[loginName] The login name component of the credentials
+ * @property[passwordHash] The hash of the password component of the credentials
+ */
 object Credentials : Table<CredentialDTO>("Credential") {
     val userId = int("userId").primaryKey().references(Users) { it.user }
     var loginName = varchar("username").bindTo { it.loginName }
     var passwordHash = varchar("password").bindTo { it.passwordHash }
 }
 
+/**
+ * A sequence of [CredentialDTO]
+ */
 val Database.credentials get() = this.sequenceOf(Credentials)
--- src/main/kotlin/nomic/data/dtos/UserDTO.kt
+++ src/main/kotlin/nomic/data/dtos/UserDTO.kt
@@ -7,15 +7,33 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
 
+/**
+ * This DTO represents a user entry in the database and employs KTorm's framework
+ *
+ * @see[org.ktorm.entity.Entity]
+ * @see[nomic.domain.entities.User]
+ * @property[id] Represents the primary key id of the user
+ * @property[name] Represents the name of the user
+ */
 interface UserDTO : Entity<UserDTO> {
     companion object : Entity.Factory<UserDTO>()
     var id: Int
     var name: String
 }
 
+/**
+ * This object represents the users table in the database, employing KTorm's framework.
+ *
+ * @see[org.ktorm.schema.Table]
+ * @property[id] The primary key for [UserDTO]
+ * @property[name] The name of the user
+ */
 object Users : Table<UserDTO>("User") {
     val id = int("userId").primaryKey().bindTo { it.id }
     val name = varchar("name").bindTo { it.name }
 }
 
+/**
+ * A sequence of [UserDTO]
+ */
 val Database.users get() = this.sequenceOf(Users)
