7504720cc323895fc9d1c4c6d57671181ca83a3c
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-22 15:53:57 -0600

    Seeds data for login tests

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+-- Login tests data
+
+INSERT INTO `User` VALUES (1, 'Foo Bar Jr.');
+INSERT INTO `Credential` VALUES ('TestUser', '$argon2id$v=19$m=16384,t=2,p=1$UUtqVlF6Wk1adkxmaXdKQQ$/6PJsEAGnyNRx1/b9/q7OWBp6SvKXIajQVRuswKQIFA', 1);
