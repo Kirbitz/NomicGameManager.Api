4771e7b24f918b7d8b0b7817e66d5e4e2833ceff
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-23 13:51:44 -0600

    Documents Spring Boot entry point

--- src/main/kotlin/nomic/ApiApplication.kt
+++ src/main/kotlin/nomic/ApiApplication.kt
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
+/**
+ * This class serves as the entry point for the Spring Boot application.
+ *
+ * This only needs to be referenced starting the server in the main function.
+ */
 @SpringBootApplication
 @EnableConfigurationProperties(
     JWTTokenConfigurationProperties::class,
