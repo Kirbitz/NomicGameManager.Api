96cb5ac0b2ca0c05d6a304bb29fee77da0e87354
TolkienFan17 <66689498+TolkienFan17@users.noreply.github.com>
2023-02-23 20:42:38 -0600

    Adds a KTLint Gradle Plugin (#8)
    * Adds ktlint gradle plugin
    
    * Formats build file
    
    * Updates plugin to use same version of linter as CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,28 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.2"
-	id("io.spring.dependency-management") version "1.1.0"
-	id("org.jetbrains.dokka") version "1.7.20"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
-	jacoco
+    id("com.diffplug.spotless") version "6.15.0"
+    id("org.springframework.boot") version "3.0.2"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.jetbrains.dokka") version "1.7.20"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+    jacoco
 }
 
 subprojects {
-	apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "org.jetbrains.dokka")
 }
 
 jacoco {
-	toolVersion = "0.8.8"
-	reportsDirectory.set(layout.buildDirectory.dir("$buildDir/jacoco"))
+    toolVersion = "0.8.8"
+    reportsDirectory.set(layout.buildDirectory.dir("$buildDir/jacoco"))
+}
+
+spotless {
+    kotlin {
+        ktlint("0.46.1") // Same version that CI uses
+    }
 }
 
 group = "game.manager.nomic"
@@ -23,38 +30,38 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("mysql:mysql-connector-java:8.0.25")
-	implementation("org.ktorm:ktorm-support-mysql:3.6.0")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("com.h2database:h2")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("mysql:mysql-connector-java:8.0.25")
+    implementation("org.ktorm:ktorm-support-mysql:3.6.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.h2database:h2")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.test {
-	finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
-	reports {
-		xml.required.set(true)
-	}
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
 }
