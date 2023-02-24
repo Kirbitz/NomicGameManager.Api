16e977d09d918f2b244c6f5d2491a114abf2c7e5
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-23 18:18:32 -0600

    Documents repo exception

--- src/main/kotlin/nomic/data/EntityNotFoundException.kt
+++ src/main/kotlin/nomic/data/EntityNotFoundException.kt
@@ -1,3 +1,7 @@
 package nomic.data
 
+/**
+ * This exception is thrown when an id for an invalid [nomic.domain.entities.Entity][Entity] is provided to a [Repository]
+ * to act upon but does not exist in the database.
+ */
 class EntityNotFoundException(id: Int) : Exception("The entity with id $id was not found on the database.")
