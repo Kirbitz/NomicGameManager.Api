0d6732448981ec84644d509c935959278bfcd465
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-24 15:09:39 -0600

    Documents basic auth header object

--- src/main/kotlin/nomic/api/auth/BasicAuthenticationHeader.kt
+++ src/main/kotlin/nomic/api/auth/BasicAuthenticationHeader.kt
@@ -2,4 +2,11 @@ package nomic.api.auth
 
 import nomic.domain.entities.LoginName
 
+/**
+ * This object represents the decoded HTTP Basic Authorization header, containing a parsed
+ * login name and password.
+ *
+ * @param[loginName] The login name which the user is trying to authenticate with
+ * @param[password] The password which the user is trying to authenticate with
+ */
 data class BasicAuthenticationHeader(val loginName: LoginName, val password: String)
