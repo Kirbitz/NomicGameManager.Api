80ce091183b7d17f0e3aedb856406021a2d5f93a
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-24 15:03:00 -0600

    Documents the argument resolver class

--- src/main/kotlin/nomic/config/resolvers/ArgumentResolverConfig.kt
+++ src/main/kotlin/nomic/config/resolvers/ArgumentResolverConfig.kt
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+/**
+ * This configuration class adds the various argument resolvers to Spring.
+ *
+ * @see[import org.springframework.web.method.support.HandlerMethodArgumentResolver]
+ */
 @Configuration
 class ArgumentResolverConfig : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
