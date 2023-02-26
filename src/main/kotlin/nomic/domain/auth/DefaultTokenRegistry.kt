44e6824ea75f847ba053e1c2828d44c72143be69
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-02-26 13:43:36 -0600

    Cleans up the token registry, adds an audience and not before claim, and adds leeway into the time claims

--- src/main/kotlin/nomic/domain/auth/DefaultTokenRegistry.kt
+++ src/main/kotlin/nomic/domain/auth/DefaultTokenRegistry.kt
@@ -2,6 +2,7 @@ package nomic.domain.auth
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.RegisteredClaims
 import com.auth0.jwt.algorithms.Algorithm
 import com.auth0.jwt.exceptions.JWTVerificationException
 import nomic.data.repositories.IUserRepository
@@ -29,16 +30,21 @@ class DefaultTokenRegistry(
         val keyPair = keyProvider.getKeyPair()
         algorithm = Algorithm.RSA256(keyPair.publicKey, keyPair.privateKey)
         verifier = JWT.require(algorithm)
-            .withIssuer("NomicGameManager.Api")
+            .withIssuer(JWT_SERVER)
+            .withAudience(JWT_SERVER)
+            .withClaimPresence(RegisteredClaims.SUBJECT)
+            .acceptLeeway(10) // Issued at, Expires at, and not before as a 10 second window of leeway
             .build()
     }
 
     override fun issueToken(user: User, claims: Map<String, String>): String {
         val tokenBuilder = JWT.create()
             .withSubject(user.id.toString())
-            .withIssuer("NomicGameManager.Api")
+            .withIssuer(JWT_SERVER)
+            .withAudience(JWT_SERVER)
             .withIssuedAt(Instant.now())
             .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+            .withNotBefore(Instant.now())
 
         for (claim in claims) {
             tokenBuilder.withClaim(claim.key, claim.value)
@@ -48,18 +54,21 @@ class DefaultTokenRegistry(
     }
 
     override fun validateToken(rawToken: String): TokenValidationResult {
-        // TODO Refactor out the exception, possibly swap out jwt libraries
+        // TODO Refactor out the exception pattern, possibly swap out jwt libraries
         try {
             val jwt = verifier.verify(rawToken)
 
-            // TODO Possibly reconsider whether this is safe.
-            // While the signing should verify integrity and since only we sign, meaning the subject should be a valid user id,
-            // Do we still need to validate the subject as an User instead of assuming?
+            // Since the signature has been verified and the token validated, we know that we created the token.
+            // Therefore, the subject claim represents a valid user id.
             val user = usersRepo.getById(jwt.subject.toInt()).get()
             val claims = jwt.claims.mapValues { it.value.asString() }
             return TokenValidationResult(true, user, claims)
         } catch (e: JWTVerificationException) {
-            return TokenValidationResult(false, null, mapOf())
+            return TokenValidationResult(false)
         }
+    }
+
+    companion object {
+        private const val JWT_SERVER = "NomicGameManager.Api"
     }
 }
