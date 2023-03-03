26ae5789e46637199d3055c3890c24c067fa5aa2
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-03-03 14:41:16 -0600

    Adds extra users for testing

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,6 +1,6 @@
 -- Login tests data
 
-INSERT INTO `User` VALUES (1, 'Foo Bar Jr.');
+INSERT INTO `User` VALUES (0, 'Cincinnatus'), (1, 'Foo Bar Jr.'), (3, 'Alcibiades');
 INSERT INTO `Credential` VALUES ('TestUser', '$argon2id$v=19$m=16384,t=2,p=1$UUtqVlF6Wk1adkxmaXdKQQ$/6PJsEAGnyNRx1/b9/q7OWBp6SvKXIajQVRuswKQIFA', 1);
 
 
