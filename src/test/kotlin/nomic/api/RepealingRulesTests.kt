a577953cbfc50e2920a74c08a5dec6339cc6069c
Adam Lucka <adamlucka15@gmail.com>
2023-03-07 14:28:13 -0600

    alphabets are not that hard, this program is just an idiot

--- src/test/kotlin/nomic/api/RepealingRulesTests.kt
+++ src/test/kotlin/nomic/api/RepealingRulesTests.kt
@@ -4,8 +4,8 @@ import nomic.domain.entities.RepealRuleResponse
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.web.client.exchange
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.exchange
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 
