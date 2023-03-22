605c56218569dd6fe4131082aac725c216ebeff6
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-03-22 16:03:40 -0500

    Adds test for Basic auth filter

---
+++ src/test/kotlin/nomic/config/security/filters/BasicAuthenticationFilterTest.kt
@@ -0,0 +1,124 @@
+package nomic.config.security.filters
+
+import nomic.domain.auth.AuthenticationResult
+import nomic.domain.auth.IUserAuthenticator
+import nomic.domain.entities.LoginName
+import nomic.domain.entities.User
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.springframework.http.HttpHeaders
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.Base64
+
+class BasicAuthenticationFilterTest {
+    private val login1 = Pair(LoginName("Alice"), "alicepassword")
+    private val user1 = User(100, "Sauron")
+
+    private val login2 = Pair(LoginName("Bob"), "bobpassword")
+    private val user2 = User(12, "Saruman")
+
+    private val invalidLogin = Pair(LoginName("Eve"), "evepassword")
+
+    val authenticator = mock<IUserAuthenticator> {
+        on {
+            authenticateUserWithCredentials(login1.first, login1.second)
+        } doReturn AuthenticationResult(true, user1)
+        on {
+            authenticateUserWithCredentials(login2.first, login2.second)
+        } doReturn AuthenticationResult(true, user2)
+        on {
+            authenticateUserWithCredentials(invalidLogin.first, invalidLogin.second)
+        } doReturn AuthenticationResult(false)
+    }
+
+    val mockMvc = MockMvcBuilders
+        .standaloneSetup(TestController())
+        .addFilter<StandaloneMockMvcBuilder>(BasicAuthenticationSecurityFilter(authenticator))
+        .build()
+
+    @BeforeEach
+    fun init() {
+        // Hacky solution to ensure that the security context doesn't persist between tests
+        SecurityContextHolder.clearContext()
+    }
+
+    private fun encode(pair: Pair<LoginName, String>): String {
+        return String(Base64.getEncoder().encode("${pair.first.rawName}:${pair.second}".toByteArray()))
+    }
+
+    @Test
+    fun filter_validEndpoint_validLogin_succeeds() {
+        val request1 = MockMvcRequestBuilders.get("/api/auth/token")
+            .header(HttpHeaders.AUTHORIZATION, "Basic ${encode(login1)}")
+
+        mockMvc.perform(request1)
+            .andExpect(content().string(user1.id.toString()))
+            .andExpect(status().isOk)
+
+        val request2 = MockMvcRequestBuilders.get("/api/auth/token")
+            .header(HttpHeaders.AUTHORIZATION, "Basic ${encode(login2)}")
+
+        mockMvc.perform(request2)
+            .andExpect(status().isOk)
+            .andExpect(content().string(user2.id.toString()))
+    }
+
+    @Test
+    fun filter_validEndpoint_invalidLogin_fails() {
+        val request = MockMvcRequestBuilders.get("/api/auth/token")
+            .header(HttpHeaders.AUTHORIZATION, "Basic ${encode(invalidLogin)}")
+
+        mockMvc.perform(request)
+            .andExpect(status().isOk)
+            .andExpect(content().string("null"))
+    }
+
+    @Test
+    fun filter_invalidEndpoint_validLogin_fails() {
+        val request = MockMvcRequestBuilders.get("/test")
+            .header(HttpHeaders.AUTHORIZATION, "Basic ${encode(login1)}")
+
+        mockMvc.perform(request)
+            .andExpect(content().string("null"))
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun filter_invalidEndpoint_invalidLogin_fails() {
+        val request = MockMvcRequestBuilders.get("/test")
+            .header(HttpHeaders.AUTHORIZATION, "Basic ${encode(invalidLogin)}")
+
+        mockMvc.perform(request)
+            .andExpect(content().string("null"))
+            .andExpect(status().isOk)
+    }
+
+    @RestController
+    private class TestController {
+
+        private fun core(): String {
+            val context = SecurityContextHolder.getContext()
+            val user = context.authentication?.principal as User?
+            return user?.id.toString()
+        }
+
+        @GetMapping("/api/auth/token")
+        fun testValid(): String {
+            return core()
+        }
+
+        @GetMapping("/test")
+        fun testInvalid(): String {
+            return core()
+        }
+    }
+}
