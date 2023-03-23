ba9e120cd3d18f96d8d1c8bc4b568093d9b295e9
Tyler Marefke <tyler.marefke@gmail.com>
2023-03-23 11:13:46 -0500

    Added basic tests for api layer

---
+++ src/test/kotlin/nomic/unit/GamesEndpointTests.kt
@@ -0,0 +1,40 @@
+package nomic.unit
+
+import nomic.api.GamesEndpoint
+import nomic.api.models.ResponseFormat
+import nomic.domain.entities.GameModel
+import nomic.domain.games.GameDomain
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.springframework.http.HttpStatus
+import java.time.LocalDate
+
+class GamesEndpointTests {
+    private val gameEndpoint: GamesEndpoint
+    private val gameDomainMock: GameDomain = mock()
+
+    init {
+        gameEndpoint = GamesEndpoint(gameDomainMock)
+    }
+
+    @Test
+    fun `Create Game with Game Object Provided and Proper Response Object Returned`() {
+        val gameInput = GameModel(13, "Cool Title Bro", LocalDate.now(), null, 42)
+
+        val result = gameEndpoint.createGame(gameInput).body as ResponseFormat
+
+        Assertions.assertThat(result.success).isTrue
+        Assertions.assertThat(result.status).isEqualTo(HttpStatus.CREATED)
+        Assertions.assertThat(result.data.toString()).contains("Game Created")
+    }
+
+    @Test
+    fun `Delete Game with Id Provided and Proper Response Object Returned`() {
+        val result = gameEndpoint.deleteGame("1234").body as ResponseFormat
+
+        Assertions.assertThat(result.success).isTrue
+        Assertions.assertThat(result.status).isEqualTo(HttpStatus.ACCEPTED)
+        Assertions.assertThat(result.data.toString()).contains("Game Deleted")
+    }
+}
---
+++ src/test/kotlin/nomic/unit/RulesAmendmentsEndpointTests.kt
@@ -0,0 +1,72 @@
+package nomic.unit
+
+import nomic.api.RulesAmendmentsEndpoint
+import nomic.api.models.AmendmentModel
+import nomic.api.models.ResponseFormat
+import nomic.api.models.RulesAmendmentsApiModel
+import nomic.domain.entities.RulesModel
+import nomic.domain.rulesamendments.RuleAmendmentDomain
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.springframework.http.HttpStatus
+
+class RulesAmendmentsEndpointTests {
+    private val rulesAmendmentsEndpoint: RulesAmendmentsEndpoint
+
+    init {
+        val ruleAmendmentList = mutableListOf(
+            RulesAmendmentsApiModel(
+                1234,
+                200,
+                "My Awesome Rule",
+                "Wow this rule is cool",
+                false,
+                mutableListOf(
+                    AmendmentModel(
+                        420,
+                        100101,
+                        "Nevermind this rule sucks",
+                        "Amend the Crappy Rule"
+                    )
+                )
+            )
+        )
+
+        val ruleAmendmentDomainMock: RuleAmendmentDomain = mock {
+            on { getRulesAmendments(anyString()) } doReturn ruleAmendmentList
+        }
+
+        rulesAmendmentsEndpoint = RulesAmendmentsEndpoint(ruleAmendmentDomainMock)
+    }
+
+    @Test
+    fun `Get Rule and Amendments with Game Id Provided and Proper Response Object Returned`() {
+        val result = rulesAmendmentsEndpoint.getRulesAmendments("2134").body as ResponseFormat
+
+        Assertions.assertThat(result.success).isTrue
+        Assertions.assertThat(result.status).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(result.data.toString()).contains("My Awesome Rule")
+    }
+
+    @Test
+    fun `Enact Rule with Rule Object Provided and Proper Response Object Returned`() {
+        val ruleInput = RulesModel(1234, 666, "Wow", "Cool Description", false, 202)
+        val result = rulesAmendmentsEndpoint.enactRule(ruleInput).body as ResponseFormat
+
+        Assertions.assertThat(result.success).isTrue
+        Assertions.assertThat(result.status).isEqualTo(HttpStatus.CREATED)
+        Assertions.assertThat(result.data.toString()).contains("Rule Created")
+    }
+
+    @Test
+    fun `Repeal Rule with Rule Id Provided and Proper Response Object Returned`() {
+        val result = rulesAmendmentsEndpoint.repealRule("4321").body as ResponseFormat
+
+        Assertions.assertThat(result.success).isTrue
+        Assertions.assertThat(result.status).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(result.data.toString()).contains("Rule Repealed")
+    }
+}
