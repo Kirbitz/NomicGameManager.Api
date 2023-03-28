5991eb84c49d158d778cf9880c1fd21febba52ac
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-03-28 14:01:43 -0500

    Removes create user function

--- src/main/kotlin/nomic/domain/auth/UserAuthenticator.kt
+++ src/main/kotlin/nomic/domain/auth/UserAuthenticator.kt
@@ -24,19 +24,6 @@ interface IUserAuthenticator {
      * @return An object containing the success state of the authentication and the user who has been authenticated
      */
     fun authenticateUserWithCredentials(loginName: LoginName, password: String): AuthenticationResult
-
-    /**
-     * Creates a new user with the provided name and credentials.
-     *
-     * Per business rules, this function will fail if the login name is not unique or if the password does not pass specified
-     * password strength rules. Otherwise, this function will create the user and return a result object with the new user.
-     *
-     * @param[name] The user's personal name - no associated business rules regarding valid names
-     * @param[loginName] The user's proposed login name that must be unique
-     * @param[password] The user's proposed password that must pass strength tests
-     * @return An object containing the success state of the account creation and the user account who has been created
-     */
-    fun createUser(name: String, loginName: LoginName, password: String): AuthenticationResult
 }
 
 /**
@@ -72,16 +59,6 @@ class Argon2UserAuthenticator(
         } else {
             return AuthenticationResult(false)
         }
-    }
-
-    override fun createUser(name: String, loginName: LoginName, password: String): AuthenticationResult {
-        /*val encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8()
-        val passwordHash = PasswordHash(encoder.encode(password))
-
-        val user = users.create(name)
-        creds.create(user, loginName, passwordHash)
-        return AuthenticationResult(true, tokenRegistry.issueToken(user))*/
-        TODO("Not yet fully implemented - to be completed in NOM-56")
     }
 
     private fun checkUserCredentials(credential: Credential, password: String): Boolean {
