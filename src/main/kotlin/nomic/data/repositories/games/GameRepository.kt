39fbd0030e4931e4aba593521509efb504ee2b91
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-04-11 14:42:37 -0500

    Adds new method to repository to retrieve a user's most recent games with pagination support

--- src/main/kotlin/nomic/data/repositories/games/GameRepository.kt
+++ src/main/kotlin/nomic/data/repositories/games/GameRepository.kt
@@ -2,13 +2,22 @@ package nomic.data.repositories.games
 
 import nomic.data.EntityNotFoundException
 import nomic.data.dtos.Games
+import nomic.data.dtos.games
+import nomic.domain.entities.EndUser
 import nomic.domain.entities.GameModel
 import org.ktorm.database.Database
 import org.ktorm.dsl.delete
 import org.ktorm.dsl.eq
 import org.ktorm.dsl.insert
+import org.ktorm.entity.count
+import org.ktorm.entity.drop
+import org.ktorm.entity.filter
+import org.ktorm.entity.map
+import org.ktorm.entity.sortedBy
+import org.ktorm.entity.take
 import org.springframework.stereotype.Repository
 import java.time.LocalDate
+import java.util.Optional
 
 /**
  * Implementation of the [IGameRepository][nomic.data.repositories.games.IGameRepository] uses
@@ -29,6 +38,28 @@ class GameRepository(private val db: Database) : IGameRepository {
             // userId will need to be changed to use the token value
             set(it.userId, input.userId)
         }
+    }
+
+    override fun listGames(user: EndUser, size: UInt, offset: UInt): Optional<List<GameModel>> {
+        if (db.games.count { it.userId eq user.id }.toUInt() < offset) {
+            return Optional.empty()
+        }
+
+        return Optional.of(
+            db.games.filter { it.userId eq user.id }
+                .drop(offset.toInt())
+                .take(size.toInt())
+                .sortedBy { it.createDate }
+                .map {
+                    GameModel(
+                        it.gameId,
+                        it.title,
+                        it.createDate,
+                        it.currentPlayer,
+                        it.user.id
+                    )
+                }.toList()
+        )
     }
 
     override fun deleteGame(gameId: Int) {
--- src/main/kotlin/nomic/data/repositories/games/IGameRepository.kt
+++ src/main/kotlin/nomic/data/repositories/games/IGameRepository.kt
@@ -1,6 +1,8 @@
 package nomic.data.repositories.games
 
+import nomic.domain.entities.EndUser
 import nomic.domain.entities.GameModel
+import java.util.Optional
 
 /**
  * Interface manipulates game data in the DB
@@ -15,6 +17,17 @@ interface IGameRepository {
      * @param input Model of the [GameModel][nomic.domain.entities.GameModel] to be made
      */
     fun createGame(input: GameModel)
+
+    /**
+     * Retrives the specified user's most recent games in descending order
+     *
+     * @param[user] The user who created the games
+     * @param[size] The maximum number of games returned
+     * @param[offset] How many games to skip to allow for pagination.
+     * @return An empty optional if there were no valid games based on the offset,
+     * otherwise a list up to @see[size] games in descending order
+     */
+    fun listGames(user: EndUser, size: UInt, offset: UInt): Optional<List<GameModel>>
 
     /**
      * Deletes the specified game from the DB
