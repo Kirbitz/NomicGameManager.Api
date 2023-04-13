2a0e851b3c38c9e9e4c02a2ff3a9ec85f472afe1
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-04-13 10:44:41 -0500

    Refactors tests to reflect repository change, and changes test entity ids to avoid conflict

--- src/integrations/kotlin/nomic/data/repositories/games/GameRepositoryTest.kt
+++ src/integrations/kotlin/nomic/data/repositories/games/GameRepositoryTest.kt
@@ -1,18 +1,26 @@
 package nomic.data.repositories.games
 
+import nomic.data.dtos.GameDTO
+import nomic.data.dtos.Games
+import nomic.data.dtos.games
 import nomic.domain.entities.EndUser
 import nomic.domain.entities.GameModel
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 import org.ktorm.database.Database
+import org.ktorm.dsl.eq
+import org.ktorm.entity.EntitySequence
+import org.ktorm.entity.filter
+import org.ktorm.entity.map
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import java.time.LocalDate
-import java.util.stream.Stream
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @SpringBootTest(
@@ -23,170 +31,77 @@ class GameRepositoryTest(@Autowired private val db: Database) {
     private companion object {
         private val user1: EndUser = EndUser(50, "ListGamesUser1")
         private val user2: EndUser = EndUser(55, "ListGamesUser2")
-
-        @JvmStatic
-        fun test_listGames_badOffset_emptyOptional_Source(): Stream<Arguments> {
-            return Stream.of(Arguments.of(user1, 5), Arguments.of(user2, 4))
-        }
-
-        @JvmStatic
-        fun test_listGames_filtersByUser_Source(): Stream<EndUser> {
-            return Stream.of(user1, user2)
-        }
-
-        @JvmStatic
-        fun test_listGames_skipsValidOffsets_Source(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of(user1, intArrayOf(51)),
-                Arguments.of(user1, intArrayOf(51, 52)),
-                Arguments.of(user1, intArrayOf(51, 52, 54)),
-                Arguments.of(user2, intArrayOf(56)),
-                Arguments.of(user2, intArrayOf(56, 57))
-            )
-        }
-
-        @JvmStatic
-        fun test_listGames_takesValidSize_Source(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of(user1, 1),
-                Arguments.of(user1, 2),
-                Arguments.of(user1, 3),
-                Arguments.of(user1, 4),
-                Arguments.of(user2, 1),
-                Arguments.of(user2, 2),
-                Arguments.of(user2, 3)
-            )
-        }
-
-        @JvmStatic
-        fun test_listGames_partialList_InvalidSize_Source(): Stream<Arguments> {
-            return Stream.of(Arguments.of(user1, 20, 4), Arguments.of(user2, 10, 3))
-        }
-
-        @JvmStatic
-        fun test_listGames_dataFromDatabase_Source(): Stream<Arguments> {
-            return Stream.of(
-                Arguments.of(
-                    user1,
-                    arrayOf(
-                        GameModel(51, "Foo1", LocalDate.parse("2023-01-01"), null, 50),
-                        GameModel(52, "Foo2", LocalDate.parse("2023-01-02"), null, 50),
-                        GameModel(53, "Foo3", LocalDate.parse("2023-01-04"), null, 50),
-                        GameModel(54, "Foo4", LocalDate.parse("2023-01-03"), null, 50)
-                    )
-                ),
-                Arguments.of(
-                    user2,
-                    arrayOf(
-                        GameModel(56, "Bar1", LocalDate.parse("2023-02-01"), null, 55),
-                        GameModel(57, "Bar2", LocalDate.parse("2023-02-02"), null, 55),
-                        GameModel(58, "Bar4", LocalDate.parse("2023-02-03"), null, 55)
-                    )
-                )
-            )
-        }
-
-        @JvmStatic
-        fun test_listGames_descendingOrder_Source(): Stream<EndUser> {
-            return Stream.of(user1, user2)
-        }
     }
 
-    @ParameterizedTest
-    @MethodSource("test_listGames_badOffset_emptyOptional_Source")
-    fun test_listGames_badOffset_emptyOptional(user: EndUser, offset: UInt) {
+    @Test
+    fun test_listGames_dataFromDatabase() {
         val repo = GameRepository(db)
-        val size = 100u
+        val expectedGames = db.games.map { GameModel(it.gameId, it.title, it.createDate, it.currentPlayer, it.user.id) }
+            .associateBy { it.gameId }.toMutableMap()
 
-        val games = repo.listGames(user, size, offset)
+        val games = repo.listGames()
 
-        Assertions.assertThat(games).isEmpty
-    }
-
-    @ParameterizedTest
-    @MethodSource("test_listGames_filtersByUser_Source")
-    fun test_listGames_filtersByUser(user: EndUser) {
-        val repo = GameRepository(db)
-        val size = 100u
-        val offset = 0u
-
-        val games = repo.listGames(user, size, offset)
-
-        Assertions.assertThat(games.get()).allMatch { it.userId == user.id }
-    }
-
-    @ParameterizedTest
-    @MethodSource("test_listGames_skipsValidOffsets_Source")
-    fun test_listGames_skipsValidOffsets(user: EndUser, offsetGames: IntArray) {
-        val repo = GameRepository(db)
-        val size = 100u
-
-        val games = repo.listGames(user, size, offsetGames.size.toUInt())
-
-        Assertions.assertThat(games.get()).noneMatch { offsetGames.contains(it.gameId!!) }
-    }
-
-    @ParameterizedTest
-    @MethodSource("test_listGames_takesValidSize_Source")
-    fun test_listGames_takesValidSize(user: EndUser, size: UInt) {
-        val repo = GameRepository(db)
-        val offset = 0u
-
-        val games = repo.listGames(user, size, offset)
-
-        Assertions.assertThat(games.get().size.toUInt()).isEqualTo(size)
-    }
-
-    @ParameterizedTest
-    @MethodSource("test_listGames_partialList_InvalidSize_Source")
-    fun test_listGames_partialList_InvalidSize(user: EndUser, expectedSize: UInt, actualSize: UInt) {
-        val repo = GameRepository(db)
-        val offset = 0u
-
-        val games = repo.listGames(user, expectedSize, offset)
-
-        Assertions.assertThat(games.get().size.toUInt()).isEqualTo(actualSize)
-    }
-
-    @ParameterizedTest
-    @MethodSource("test_listGames_dataFromDatabase_Source")
-    fun test_listGames_dataFromDatabase(user: EndUser, expectedGames: Array<GameModel>) {
-        val repo = GameRepository(db)
-        val offset = 0u
-        val size = 100u
-
-        val games = repo.listGames(user, size, offset)
-
-        val expectedGamesMap = expectedGames.associateBy { it.gameId }
-        val gamesMap = games.get().associateBy { it.gameId }
-        val doMatch = { model1: GameModel, model2: GameModel ->
-            val result = model1.gameId == model2.gameId &&
-                model1.userId.equals(model2.userId) &&
-                model1.title.equals(model2.title) &&
-                model1.createDate == model2.createDate &&
-                model1.currentPlayer == model2.currentPlayer
-            result
-        }
-
-        Assertions.assertThat(games.get()).allMatch {
-            doMatch(it, expectedGamesMap[it.gameId]!!)
-        }
-        Assertions.assertThat(expectedGames).allMatch { doMatch(it, gamesMap[it.gameId]!!) }
-    }
-
-    @ParameterizedTest
-    @MethodSource("test_listGames_descendingOrder_Source")
-    fun test_listGames_descendingOrder(user: EndUser) {
-        val repo = GameRepository(db)
-        val size = 100u
-        val offset = 0u
-
-        val games = repo.listGames(user, size, offset).get()
-
-        var lastDate = games.first().createDate
         for (game in games) {
-            Assertions.assertThat(lastDate).isBeforeOrEqualTo(game.createDate)
-            lastDate = game.createDate
+            Assertions.assertThat(expectedGames).containsKey(game.gameId)
+            val expectedGame = expectedGames[game.gameId]!!
+
+            Assertions.assertThat(game.createDate).isEqualTo(expectedGame.createDate)
+            Assertions.assertThat(game.userId).isEqualTo(expectedGame.userId)
+            Assertions.assertThat(game.title).isEqualTo(expectedGame.title)
+            Assertions.assertThat(game.currentPlayer).isEqualTo(expectedGame.currentPlayer)
+
+            expectedGames.remove(game.gameId)
         }
+
+        Assertions.assertThat(expectedGames.size).isZero()
+    }
+
+    @Test
+    fun test_listGames_callsSpecificationsInOrder() {
+        val repo = GameRepository(db)
+        val specUser = IGameSpecification { games -> games.filter { it.userId eq 1601 } }
+
+        var hasFirstSpecRun = false
+
+        val firstSpec = IGameSpecification {
+                games ->
+            hasFirstSpecRun = true
+            games
+        }
+
+        val secondSpec = IGameSpecification {
+                games ->
+            Assertions.assertThat(hasFirstSpecRun).isTrue
+            games
+        }
+
+        val games = repo.listGames(specUser, firstSpec, secondSpec)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 4, 8, 16])
+    fun test_listGames_callsAllSpecifications(specificationCount: Int) {
+        val repo = GameRepository(db)
+        val mockSpecifications = mutableListOf<IGameSpecification>()
+        var totalSpecificationCalls = 0
+
+        for (i in 1..specificationCount) {
+            mockSpecifications.add(
+                mock<IGameSpecification> {
+                    on(it.apply(any<EntitySequence<GameDTO, Games>>())) doAnswer {
+                            answer ->
+                        totalSpecificationCalls++
+                        answer.getArgument(0)
+                    }
+                }
+            )
+        }
+
+        // Filter for the performance of the test so that the repo isn't processing a bunch of entities
+        val filterByUser = IGameSpecification { games -> games.filter { it.userId eq 1601 } }
+
+        repo.listGames(filterByUser, *mockSpecifications.toTypedArray())
+
+        Assertions.assertThat(totalSpecificationCalls).isEqualTo(specificationCount)
     }
 }
