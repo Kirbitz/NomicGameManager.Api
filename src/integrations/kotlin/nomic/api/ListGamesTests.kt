3d420e98f7bb6644cf6a71c39c8a23ce3feb3f16
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-04-15 22:12:42 -0500

    Expands end-to-end test coverage for the list games endpoint

--- src/integrations/kotlin/nomic/api/ListGamesTests.kt
+++ src/integrations/kotlin/nomic/api/ListGamesTests.kt
@@ -52,4 +52,59 @@ class ListGamesTests(
         Assertions.assertThat(data[0].gameId).isEqualTo(1622)
         Assertions.assertThat(data[1].gameId).isEqualTo(1621)
     }
+
+    @Test
+    fun test_listGames_invalidOffset() {
+        val user = EndUser(1620, "ListGamesUser2")
+        val request = createRequest(ListGamesApiRequestModel(2U, 10U), user)
+        val entity = client.exchange<ResponseFormat<String>>("/api/game/list", HttpMethod.GET, request)
+
+        Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+
+        Assertions.assertThat(entity.body?.success).isFalse
+        Assertions.assertThat(entity.body?.status).isEqualTo(HttpStatus.NOT_FOUND)
+
+        // Case-insensitive of starting letter
+        Assertions.assertThat(entity.body?.data).contains("ffset")
+    }
+
+    @Test
+    fun test_listGames_tooSmallSize() {
+        val user = EndUser(1620, "ListGamesUser2")
+        val request = createRequest(ListGamesApiRequestModel(0U, 0U), user)
+        val entity = client.exchange<ResponseFormat<String>>("/api/game/list", HttpMethod.GET, request)
+
+        Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+
+        Assertions.assertThat(entity.body?.success).isFalse
+        Assertions.assertThat(entity.body?.status).isEqualTo(HttpStatus.BAD_REQUEST)
+        Assertions.assertThat(entity.body?.data).contains("1")
+    }
+
+    @Test
+    fun test_listGames_tooLargeSize() {
+        val user = EndUser(1620, "ListGamesUser2")
+        val request = createRequest(ListGamesApiRequestModel(1000U, 0U), user)
+        val entity = client.exchange<ResponseFormat<String>>("/api/game/list", HttpMethod.GET, request)
+
+        Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+
+        Assertions.assertThat(entity.body?.success).isFalse
+        Assertions.assertThat(entity.body?.status).isEqualTo(HttpStatus.BAD_REQUEST)
+        Assertions.assertThat(entity.body?.data).contains("100")
+    }
+
+    @Test
+    fun test_listGames_offsetOptional() {
+        val user = EndUser(1620, "ListGamesUser2")
+        val request = createRequest(object { val size: UInt = 100U }, user)
+        val entity = client.exchange<ResponseFormat<List<GameModel>>>("/api/game/list", HttpMethod.GET, request)
+
+        Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+
+        Assertions.assertThat(entity.body?.success).isTrue
+        Assertions.assertThat(entity.body?.status).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(entity.body?.data).isNotNull()
+        Assertions.assertThat(entity.body?.data!!.size).isEqualTo(3)
+    }
 }
