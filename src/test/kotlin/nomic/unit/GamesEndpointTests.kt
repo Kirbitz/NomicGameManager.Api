8d0fdd74a92845ac76a1e8bde4153af61f39664d
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-04-18 14:42:23 -0500

    Adds unit tests for the list games endpoint

--- src/test/kotlin/nomic/unit/GamesEndpointTests.kt
+++ src/test/kotlin/nomic/unit/GamesEndpointTests.kt
@@ -1,14 +1,18 @@
 package nomic.unit
 
 import nomic.api.GamesEndpoint
+import nomic.api.models.ListGamesApiRequestModel
 import nomic.api.models.ResponseFormat
+import nomic.domain.entities.EndUser
 import nomic.domain.entities.GameModel
 import nomic.domain.games.GameDomain
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.springframework.http.HttpStatus
 import java.time.LocalDate
+import java.util.Optional
 
 class GamesEndpointTests {
     private val gameEndpoint: GamesEndpoint
@@ -27,6 +31,55 @@ class GamesEndpointTests {
         Assertions.assertThat(result.success).isTrue
         Assertions.assertThat(result.status).isEqualTo(HttpStatus.CREATED)
         Assertions.assertThat(result.data).contains("Game Created")
+    }
+
+    @Test
+    fun test_listGames_validParameters() {
+        val user = EndUser(1, "Foo Bar")
+        val domain = mock<GameDomain>() {
+            on(it.listGames(user, 100U, 0U)) doReturn Optional.of(listOf())
+        }
+
+        val endpoint = GamesEndpoint(domain)
+
+        val result = endpoint.listGames(ListGamesApiRequestModel(100U, 0U), user).body as ResponseFormat<*>
+
+        Assertions.assertThat(result.success).isTrue
+        Assertions.assertThat(result.status).isEqualTo(HttpStatus.OK)
+        Assertions.assertThat(result.data).isInstanceOf(List::class.java)
+    }
+
+    @Test
+    fun test_listGames_invalidSize() {
+        val user = EndUser(1, "Foo Bar")
+        val size = 1000u
+        val domain = mock<GameDomain>() {
+            on(it.listGames(user, size, 0U)) doReturn Optional.of(listOf())
+        }
+
+        val endpoint = GamesEndpoint(domain)
+
+        val result = endpoint.listGames(ListGamesApiRequestModel(size, 0U), user).body as ResponseFormat<*>
+
+        Assertions.assertThat(result.success).isFalse
+        Assertions.assertThat(result.status).isEqualTo(HttpStatus.BAD_REQUEST)
+        Assertions.assertThat(result.data).isInstanceOf(String::class.java)
+    }
+
+    @Test
+    fun test_listGames_invalidOffset() {
+        val user = EndUser(1, "Foo Bar")
+        val domain = mock<GameDomain>() {
+            on(it.listGames(user, 100U, 0U)) doReturn Optional.empty()
+        }
+
+        val endpoint = GamesEndpoint(domain)
+
+        val result = endpoint.listGames(ListGamesApiRequestModel(100U, 0U), user).body as ResponseFormat<*>
+
+        Assertions.assertThat(result.success).isFalse
+        Assertions.assertThat(result.status).isEqualTo(HttpStatus.NOT_FOUND)
+        Assertions.assertThat(result.data).isInstanceOf(String::class.java)
     }
 
     @Test
